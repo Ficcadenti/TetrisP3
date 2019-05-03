e59aefa0b84f9b094051b6e41836163ae37dc12b
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2019-05-03 19:23:52 +0200

    Ora siamo allineati :)

--- Tetris/src/it/raffomafr/tetris/game/Tetris.java
+++ Tetris/src/it/raffomafr/tetris/game/Tetris.java
@@ -74,9 +74,7 @@ public class Tetris extends PApplet
 		{
 			this.stroke(this.random(255), this.random(255), this.random(255));
 			this.strokeWeight(this.random(0, 4));
-			this.point(Costanti.Sketch.LARGHEZZA + this.random(0, Costanti.Statistiche.LARGHEZZA_STATISTICHE),
-					this.random(0,
-							Costanti.Sketch.ALTEZZA_HEADER + Costanti.Sketch.ALTEZZA + Costanti.Sketch.ALTEZZA_FOOTER));
+			this.point(Costanti.Sketch.LARGHEZZA + this.random(0, Costanti.Statistiche.LARGHEZZA_STATISTICHE), this.random(0, Costanti.Sketch.ALTEZZA_HEADER + Costanti.Sketch.ALTEZZA + Costanti.Sketch.ALTEZZA_FOOTER));
 		}
 		this.popMatrix();
 		this.stroke(0, 0, 0);
@@ -90,8 +88,7 @@ public class Tetris extends PApplet
 		{
 			this.stroke(this.random(255), this.random(255), this.random(255));
 			this.strokeWeight(this.random(0, 4));
-			this.point(this.random(0, Costanti.Sketch.LARGHEZZA), Costanti.Sketch.ALTEZZA_HEADER
-					+ Costanti.Sketch.ALTEZZA + this.random(0, Costanti.Sketch.ALTEZZA_FOOTER));
+			this.point(this.random(0, Costanti.Sketch.LARGHEZZA), Costanti.Sketch.ALTEZZA_HEADER + Costanti.Sketch.ALTEZZA + this.random(0, Costanti.Sketch.ALTEZZA_FOOTER));
 		}
 		this.popMatrix();
 		this.stroke(0, 0, 0);
@@ -149,6 +146,7 @@ public class Tetris extends PApplet
 				{
 					this.gameOver = true;
 				}
+
 				TavoloDaGioco.getInstance().inserisciMattoncino(this.mattoncinoCasuale);
 
 				this.mattoncinoCasuale = this.prossimoMattoncinoCasuale;
@@ -188,14 +186,12 @@ public class Tetris extends PApplet
 	public void drawPunteggio()
 	{
 		this.pushMatrix();
-		img_righe = loadImage("mattoncinoBarra.jpg");
-		img_righe.resize(img_righe.width / 2 + (Costanti.RigheAbbattute.DELTA_BARRETTA * numRigheAbbattuteTotali),
-				img_righe.height);
-		this.image(img_righe, Costanti.RigheAbbattute.POS_X_BARRA, Costanti.RigheAbbattute.POS_Y_BARRA);
+		this.img_righe = this.loadImage("mattoncinoBarra.jpg");
+		this.img_righe.resize((this.img_righe.width / 2) + (Costanti.RigheAbbattute.DELTA_BARRETTA * numRigheAbbattuteTotali), this.img_righe.height);
+		this.image(this.img_righe, Costanti.RigheAbbattute.POS_X_BARRA, Costanti.RigheAbbattute.POS_Y_BARRA);
 		this.fill(255);
 		this.textAlign(PConstants.LEFT);
-		this.text(Costanti.RigheAbbattute.DESC + numRigheAbbattuteTotali, Costanti.RigheAbbattute.POS_X,
-				Costanti.RigheAbbattute.POS_Y);
+		this.text(Costanti.RigheAbbattute.DESC + numRigheAbbattuteTotali, Costanti.RigheAbbattute.POS_X, Costanti.RigheAbbattute.POS_Y);
 		this.popMatrix();
 	}
 
@@ -204,8 +200,7 @@ public class Tetris extends PApplet
 		int cont = 1;
 
 		this.pushMatrix();
-		this.rect(Costanti.Sketch.LARGHEZZA, 0, Costanti.Statistiche.LARGHEZZA_STATISTICHE,
-				Costanti.Sketch.ALTEZZA_HEADER + Costanti.Sketch.ALTEZZA + Costanti.Sketch.ALTEZZA_FOOTER);
+		this.rect(Costanti.Sketch.LARGHEZZA, 0, Costanti.Statistiche.LARGHEZZA_STATISTICHE, Costanti.Sketch.ALTEZZA_HEADER + Costanti.Sketch.ALTEZZA + Costanti.Sketch.ALTEZZA_FOOTER);
 		Set<MattonciniString> mattoncini = this.statistiche.keySet();
 		for (MattonciniString m : mattoncini)
 		{
@@ -213,15 +208,13 @@ public class Tetris extends PApplet
 			try
 			{
 				this.img_stat = (PImage) mapTetrisImg.get(m.getTipo()).clone();
+				this.img_stat.resize(this.img_stat.width, this.img_stat.height + (Costanti.Statistiche.DELTA_BARRETTA * this.statistiche.get(m)));
+				this.image(this.img_stat, (Costanti.Sketch.LARGHEZZA - 20) + (Costanti.Statistiche.INTERVALLO_BARRETTE * cont), Costanti.Statistiche.ALTEZZA - (Costanti.Statistiche.DELTA_BARRETTA * this.statistiche.get(m)));
 			}
 			catch (CloneNotSupportedException e)
 			{
 				log.info("Non posso clonare " + m.getDesc());
 			}
-			img_stat.resize(img_stat.width,
-					img_stat.height + (Costanti.Statistiche.DELTA_BARRETTA * statistiche.get(m)));
-			this.image(img_stat, (Costanti.Sketch.LARGHEZZA - 20) + Costanti.Statistiche.INTERVALLO_BARRETTE * cont,
-					Costanti.Statistiche.ALTEZZA - (Costanti.Statistiche.DELTA_BARRETTA * statistiche.get(m)));
 			cont++;
 		}
 		this.popMatrix();
@@ -241,18 +234,13 @@ public class Tetris extends PApplet
 				if (tavolo[x][y] == MattonciniString.VUOTO.getTipo())
 				{
 					this.stroke(0, 0, 0);
-					this.rect((x * Costanti.Sketch.LARGHEZZA_CELLA),
-							(Costanti.Sketch.ALTEZZA_HEADER) + (y * Costanti.Sketch.ALTEZZA_CELLA),
-							Costanti.Sketch.LARGHEZZA_CELLA, Costanti.Sketch.ALTEZZA_CELLA);
+					this.rect((x * Costanti.Sketch.LARGHEZZA_CELLA), (Costanti.Sketch.ALTEZZA_HEADER) + (y * Costanti.Sketch.ALTEZZA_CELLA), Costanti.Sketch.LARGHEZZA_CELLA, Costanti.Sketch.ALTEZZA_CELLA);
 					this.fill(20, 20, 20);
-					this.rect((x * Costanti.Sketch.LARGHEZZA_CELLA),
-							(Costanti.Sketch.ALTEZZA_HEADER) + (y * Costanti.Sketch.ALTEZZA_CELLA),
-							Costanti.Sketch.LARGHEZZA_CELLA, Costanti.Sketch.ALTEZZA_CELLA);
+					this.rect((x * Costanti.Sketch.LARGHEZZA_CELLA), (Costanti.Sketch.ALTEZZA_HEADER) + (y * Costanti.Sketch.ALTEZZA_CELLA), Costanti.Sketch.LARGHEZZA_CELLA, Costanti.Sketch.ALTEZZA_CELLA);
 				}
 				else
 				{
-					this.image(mapTetrisImg.get(tavolo[x][y]), x * Costanti.Sketch.LARGHEZZA_CELLA,
-							(Costanti.Sketch.ALTEZZA_HEADER) + (y * Costanti.Sketch.ALTEZZA_CELLA));
+					this.image(mapTetrisImg.get(tavolo[x][y]), x * Costanti.Sketch.LARGHEZZA_CELLA, (Costanti.Sketch.ALTEZZA_HEADER) + (y * Costanti.Sketch.ALTEZZA_CELLA));
 				}
 
 			}
@@ -263,15 +251,13 @@ public class Tetris extends PApplet
 	@Override
 	public void settings()
 	{
-		this.size(Costanti.Sketch.LARGHEZZA + Costanti.Statistiche.LARGHEZZA_STATISTICHE,
-				Costanti.Sketch.ALTEZZA_HEADER + Costanti.Sketch.ALTEZZA + Costanti.Sketch.ALTEZZA_FOOTER);
+		this.size(Costanti.Sketch.LARGHEZZA + Costanti.Statistiche.LARGHEZZA_STATISTICHE, Costanti.Sketch.ALTEZZA_HEADER + Costanti.Sketch.ALTEZZA + Costanti.Sketch.ALTEZZA_FOOTER);
 	}
 
 	public void caricaImg() // non mi piace, ma per velocit� (contro ogni buon proposit) lo faccio:)
 	{
 		mapTetrisImg = new HashMap<Integer, PImage>();
-		mapTetrisImg.put(new Integer(MattonciniString.MURO.getTipo()),
-				this.loadImage(MattonciniString.MURO.getNomeImg()));
+		mapTetrisImg.put(new Integer(MattonciniString.MURO.getTipo()), this.loadImage(MattonciniString.MURO.getNomeImg()));
 		mapTetrisImg.put(new Integer(MattonciniString.T.getTipo()), this.loadImage(MattonciniString.T.getNomeImg()));
 		mapTetrisImg.put(new Integer(MattonciniString.I.getTipo()), this.loadImage(MattonciniString.I.getNomeImg()));
 		mapTetrisImg.put(new Integer(MattonciniString.L.getTipo()), this.loadImage(MattonciniString.L.getNomeImg()));
@@ -279,8 +265,7 @@ public class Tetris extends PApplet
 		mapTetrisImg.put(new Integer(MattonciniString.O.getTipo()), this.loadImage(MattonciniString.O.getNomeImg()));
 		mapTetrisImg.put(new Integer(MattonciniString.S.getTipo()), this.loadImage(MattonciniString.S.getNomeImg()));
 		mapTetrisImg.put(new Integer(MattonciniString.Z.getTipo()), this.loadImage(MattonciniString.Z.getNomeImg()));
-		mapTetrisImg.put(new Integer(MattonciniString.PROIEZIONE.getTipo()),
-				this.loadImage(MattonciniString.PROIEZIONE.getNomeImg()));
+		mapTetrisImg.put(new Integer(MattonciniString.PROIEZIONE.getTipo()), this.loadImage(MattonciniString.PROIEZIONE.getNomeImg()));
 	}
 
 	@Override
@@ -288,8 +273,8 @@ public class Tetris extends PApplet
 	{
 		PropertyConfigurator.configure("log4j.properties");
 		this.textFont(this.createFont("Gugi-Regular.ttf", Costanti.sizeFont, true), Costanti.sizeFont);
-		azzeraStatistiche();
-		cursor(CROSS);
+		this.azzeraStatistiche();
+		this.cursor(CROSS);
 		GestioneBottoni.getInstance().setPa(this);
 		GestioneBottoni.getInstance().addBottone(BottoniGioco.SI);
 		GestioneBottoni.getInstance().addBottone(BottoniGioco.NO);
@@ -355,51 +340,48 @@ public class Tetris extends PApplet
 
 	public void gameOver()
 	{
-		pushMatrix();
-		fill(0);
-		noStroke();
-		rect(510, 0, width, height);
-		this.gameOver = true;
-		this.image(this.loadImage(UtilityGioco.GAMEOVER.getDesc(), UtilityGioco.GAMEOVER.getEstensione()),
-				UtilityGioco.GAMEOVER.getPosX(), UtilityGioco.GAMEOVER.getPosY(), UtilityGioco.GAMEOVER.getLarghezza(),
-				UtilityGioco.GAMEOVER.getAltezza());
+		this.pushMatrix();
 		this.fill(0);
-		rect(110, 500, 300, 150, 20);
+		this.noStroke();
+		this.rect(510, 0, this.width, this.height);
+		this.gameOver = true;
+		this.image(this.loadImage(UtilityGioco.GAMEOVER.getDesc(), UtilityGioco.GAMEOVER.getEstensione()), UtilityGioco.GAMEOVER.getPosX(), UtilityGioco.GAMEOVER.getPosY(), UtilityGioco.GAMEOVER.getLarghezza(), UtilityGioco.GAMEOVER.getAltezza());
+		this.fill(0);
+		this.rect(110, 500, 300, 150, 20);
 		this.fill(255, 255, 0);
-		centraTesto(BottoniGioco.SCRITTAGAMEOVER);
-		centraTesto(BottoniGioco.SI);
-		centraTesto(BottoniGioco.NO);
+		this.centraTesto(BottoniGioco.SCRITTAGAMEOVER);
+		this.centraTesto(BottoniGioco.SI);
+		this.centraTesto(BottoniGioco.NO);
 		this.fill(255);
-		statisticheGameOver();
+		this.statisticheGameOver();
 		GestioneBottoni.getInstance().sceltaGameOver();
-		popMatrix();
+		this.popMatrix();
 	}
 
 	private void statisticheGameOver()
 	{
-		text("Hai totalizzato : ", 540, 250);
-		image(loadImage("gameOver/mattoncino_i.png"), 540, 300, 50, 20);
-		text(statistiche.get(MattonciniString.I), 600, 320);
-		image(loadImage("gameOver/mattoncino_l.png"), 540, 350, 50, 30);
-		text(statistiche.get(MattonciniString.L), 600, 370);
-		image(loadImage("gameOver/mattoncino_j.png"), 540, 400, 50, 30);
-		text(statistiche.get(MattonciniString.J), 600, 420);
-		image(loadImage("gameOver/mattoncino_o.png"), 540, 450, 30, 30);
-		text(statistiche.get(MattonciniString.O), 600, 470);
-		image(loadImage("gameOver/mattoncino_s.png"), 540, 500, 50, 30);
-		text(statistiche.get(MattonciniString.S), 600, 520);
-		image(loadImage("gameOver/mattoncino_t.png"), 540, 550, 50, 30);
-		text(statistiche.get(MattonciniString.T), 600, 570);
-		image(loadImage("gameOver/mattoncino_z.png"), 540, 600, 50, 30);
-		text(statistiche.get(MattonciniString.Z), 600, 620);
+		this.text("Hai totalizzato : ", 540, 250);
+		this.image(this.loadImage("gameOver/mattoncino_i.png"), 540, 300, 50, 20);
+		this.text(this.statistiche.get(MattonciniString.I), 600, 320);
+		this.image(this.loadImage("gameOver/mattoncino_l.png"), 540, 350, 50, 30);
+		this.text(this.statistiche.get(MattonciniString.L), 600, 370);
+		this.image(this.loadImage("gameOver/mattoncino_j.png"), 540, 400, 50, 30);
+		this.text(this.statistiche.get(MattonciniString.J), 600, 420);
+		this.image(this.loadImage("gameOver/mattoncino_o.png"), 540, 450, 30, 30);
+		this.text(this.statistiche.get(MattonciniString.O), 600, 470);
+		this.image(this.loadImage("gameOver/mattoncino_s.png"), 540, 500, 50, 30);
+		this.text(this.statistiche.get(MattonciniString.S), 600, 520);
+		this.image(this.loadImage("gameOver/mattoncino_t.png"), 540, 550, 50, 30);
+		this.text(this.statistiche.get(MattonciniString.T), 600, 570);
+		this.image(this.loadImage("gameOver/mattoncino_z.png"), 540, 600, 50, 30);
+		this.text(this.statistiche.get(MattonciniString.Z), 600, 620);
 	}
 
 	private void centraTesto(BottoniGioco testo)
 	{
 		this.noFill();
-		this.text(testo.getDesc(), testo.getPosX() + (testo.getLarghezza() - textWidth(testo.getDesc())) / 2,
-				testo.getPosY() + ((testo.getAltezza() + Costanti.sizeFont) / 2));
-		stroke(255);
+		this.text(testo.getDesc(), testo.getPosX() + ((testo.getLarghezza() - this.textWidth(testo.getDesc())) / 2), testo.getPosY() + ((testo.getAltezza() + Costanti.sizeFont) / 2));
+		this.stroke(255);
 		this.rect(testo.getPosX(), testo.getPosY(), testo.getLarghezza(), testo.getAltezza());
 	}
 
@@ -413,11 +395,8 @@ public class Tetris extends PApplet
 				this.noLoop();
 				this.filter(GRAY);
 				this.fill(255);
-				this.image(this.loadImage(UtilityGioco.PAUSA.getDesc(), UtilityGioco.PAUSA.getEstensione()),
-						UtilityGioco.PAUSA.getPosX(), UtilityGioco.PAUSA.getPosY(), UtilityGioco.PAUSA.getLarghezza(),
-						UtilityGioco.PAUSA.getAltezza());
-				this.text(UtilityGioco.SCRITTAPAUSA.getDesc(), UtilityGioco.SCRITTAPAUSA.getPosX(),
-						UtilityGioco.SCRITTAPAUSA.getPosY());
+				this.image(this.loadImage(UtilityGioco.PAUSA.getDesc(), UtilityGioco.PAUSA.getEstensione()), UtilityGioco.PAUSA.getPosX(), UtilityGioco.PAUSA.getPosY(), UtilityGioco.PAUSA.getLarghezza(), UtilityGioco.PAUSA.getAltezza());
+				this.text(UtilityGioco.SCRITTAPAUSA.getDesc(), UtilityGioco.SCRITTAPAUSA.getPosX(), UtilityGioco.SCRITTAPAUSA.getPosY());
 			}
 			else
 			{
@@ -441,14 +420,12 @@ public class Tetris extends PApplet
 		this.stroke(100, 100, 100);
 		for (int x = 0; x <= Costanti.Sketch.LARGHEZZA; x++)
 		{
-			this.line(x * Costanti.Sketch.LARGHEZZA_CELLA, 0, x * Costanti.Sketch.LARGHEZZA_CELLA,
-					Costanti.Sketch.ALTEZZA);
+			this.line(x * Costanti.Sketch.LARGHEZZA_CELLA, 0, x * Costanti.Sketch.LARGHEZZA_CELLA, Costanti.Sketch.ALTEZZA);
 		}
 
 		for (int y = 0; y <= Costanti.Sketch.ALTEZZA; y++)
 		{
-			this.line(0, y * Costanti.Sketch.ALTEZZA_CELLA, Costanti.Sketch.LARGHEZZA,
-					y * Costanti.Sketch.ALTEZZA_CELLA);
+			this.line(0, y * Costanti.Sketch.ALTEZZA_CELLA, Costanti.Sketch.LARGHEZZA, y * Costanti.Sketch.ALTEZZA_CELLA);
 		}
 		this.popMatrix();
 	}
@@ -470,8 +447,7 @@ public class Tetris extends PApplet
 				if (mattoncino.getMatrice()[x][y] != 0)
 				{
 					xPosM = (mattoncino.getPosx() + x) * Costanti.Sketch.LARGHEZZA_CELLA;
-					this.image(mapTetrisImg.get(MattonciniString.PROIEZIONE.getTipo()), xPosM,
-							(Costanti.Sketch.ALTEZZA_HEADER) + yPosM);
+					this.image(mapTetrisImg.get(MattonciniString.PROIEZIONE.getTipo()), xPosM, (Costanti.Sketch.ALTEZZA_HEADER) + yPosM);
 				}
 			}
 		}
